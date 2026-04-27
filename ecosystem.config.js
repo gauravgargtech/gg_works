ea03d9358e8df54c30fb2b54c363287d7099f5d8
gaurav <gauravgargtech2@gmail.com>
2026-04-27 23:20:04 +1000

    ss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -49,5 +49,16 @@ module.exports = {
       watch: false,
       autorestart: true,
     },
+    {
+      name: "logs",
+      script: "./crons/pm2-betterstack-forwarder.js",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "prod",
+      },
+      watch: false,
+      autorestart: true,
+    },
   ],
 };
