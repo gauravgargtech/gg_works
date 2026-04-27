bec6171df0ff85863501710d3cf0985fad872212
gaurav <gauravgargtech2@gmail.com>
2026-04-27 13:00:23 +1000

    d

---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  apps: [
+    // MAIN API SERVER
+    {
+      name: "api",
+      script: "app.js",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "prod",
+      },
+      watch: false,
+      max_memory_restart: "300M",
+    },
+
+    // CRON 1
+    {
+      name: "cron_btc",
+      script: "./crons/profits_btc.js",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "prod",
+      },
+      watch: false,
+      autorestart: true,
+    },
+
+    // CRON 2
+    {
+      name: "cron_oanda",
+      script: "./crons/profits_forex.js",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "prod",
+      },
+      watch: false,
+      autorestart: true,
+    },
+  ],
+};
