eb4d086a947709b9df3d9ed9a8061f0d6a1f457d
gaurav <gauravgargtech2@gmail.com>
2026-04-29 21:44:36 +1000

    ss

--- config/config.js
+++ config/config.js
@@ -20,32 +20,6 @@ tracker.track({
   accessToken: process.env.MIDDLEWARE_TOKEN,
 });
 
-console.log = (...args) => {
-  tracker.info(formatArgs(args));
-};
-
-console.info = (...args) => {
-  tracker.info(formatArgs(args));
-};
-
-console.warn = (...args) => {
-  tracker.warn(formatArgs(args));
-};
-
-console.error = (...args) => {
-  tracker.error(formatArgs(args));
-};
-
-console.debug = (...args) => {
-  tracker.debug(formatArgs(args));
-};
-
-function formatArgs(args) {
-  return args
-    .map((a) => (typeof a === "object" ? JSON.stringify(a, null, 2) : a))
-    .join(" ");
-}
-
 module.exports = {
   env,
   isProd: env === "prod",
