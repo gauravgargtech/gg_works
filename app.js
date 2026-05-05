a72a184d92efcdab6ef81c23d9724bdd3c26c512
gaurav <gauravgargtech2@gmail.com>
2026-05-05 12:15:04 +1000

    mt5 signals

--- app.js
+++ app.js
@@ -151,8 +151,22 @@ app.post("/tv-webhook", async (req, res) => {
       await del("EURUSD_buy_70");
 
       if (alertParts.signal === "BUY") {
+        await set(
+          "mt5:pending_command",
+          JSON.stringify({
+            action: "replace",
+            direction: "buy",
+          }),
+        );
         await placeOrder("buy");
       } else if (alertParts.signal === "SELL") {
+        await set(
+          "mt5:pending_command",
+          JSON.stringify({
+            action: "replace",
+            direction: "sell",
+          }),
+        );
         await placeOrder("short");
       }
     } else if (alertParts?.symbol === "POPCAT") {
@@ -194,10 +208,11 @@ app.post("/tv-webhook", async (req, res) => {
 // MT5 polls this every 2 seconds
 app.get("/mt5/command", async (req, res) => {
   console.log("MT5 polling for command");
-  return res.json({ action: "none" });
 
   const cmd = await get("mt5:pending_command");
+
   if (cmd) {
+    await del("mt5:pending_command");
     return res.json(JSON.parse(cmd));
   }
   res.json({ action: "none" });
@@ -205,12 +220,13 @@ app.get("/mt5/command", async (req, res) => {
 
 // MT5 calls this after executing a command
 app.post("/mt5/ack", async (req, res) => {
-  return res.json({ status: "ok" });
   console.log("MT5 acknowledged command");
   console.log("Request");
   console.log(req);
   console.log(req.body);
   await del("mt5:pending_command");
+
+  return res.json({ status: "ok" });
   console.log("MT5 acknowledged command — queue cleared");
   res.json({ status: "ok" });
 });
