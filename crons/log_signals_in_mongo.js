8d82978e506ca9c8b8bc39252d9ecb93af142930
gaurav <gauravgargtech2@gmail.com>
2026-05-07 15:05:22 +1000

    s

--- crons/log_signals_in_mongo.js
+++ crons/log_signals_in_mongo.js
@@ -44,7 +44,7 @@ const CONFIG = {
   count: parseInt(process.env.OANDA_COUNT || "100000"), // candles to fetch (max 5000)
 
   // UT Bot parameters (your settings)
-  utKeyValue: 9, // previous value - 24, changed on May 7 2026
+  utKeyValue: 24,
   utAtrPeriod: 10,
 
   // HMA
