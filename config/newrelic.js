794ff7478a46cbeddef9c3c2681e65d3ecdb600d
gaurav <gauravgargtech2@gmail.com>
2026-05-15 13:20:37 +1000

    s

---
+++ config/newrelic.js
@@ -0,0 +1,13 @@
+"use strict";
+require("newrelic");
+exports.config = {
+  app_name: process.env.NEW_RELIC_APP_NAME,
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  logging: {
+    level: "info",
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: ["request.headers.cookie", "request.headers.authorization"],
+  },
+};
