5f897fe4b6cf0009822d890a9425251611b955e8
gaurav <gauravgargtech2@gmail.com>
2026-05-18 13:48:40 +1000

    allowing 5 pairs for forex trade

--- crons/profits_btc.js
+++ crons/profits_btc.js
@@ -84,7 +84,7 @@ function roundQty(qty, step) {
 // -----------------------------
 // 5. TP generator
 // -----------------------------
-const profitSteps = [0.005, 0.008, 0.01, 0.014, 0.018, 0.022, 0.026, 0.03];
+const profitSteps = [0.008, 0.01, 0.016, 0.22, 0.028, 0.034, 0.04, 0.05, 0.07];
 
 function buildTPs(entryPrice, isLong, mode) {
   const steps =
