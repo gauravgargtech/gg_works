b36197b04b82e81d6d2fc89bbd5b523e01f7c930
gaurav <gauravgargtech2@gmail.com>
2026-06-04 23:02:06 +1000

    d

--- crons/all_crons.js
+++ crons/all_crons.js
@@ -77,6 +77,13 @@ cron.schedule("*/5 * * * *", async () => {
   console.log("Refresh Instruments Data every 5 minutes");
 
   try {
+    await theForexPerfectMapo("M5");
+  } catch (err) {
+    console.error("Error in mapo_forex_perfect: ", err);
+    await sendPushNotif("Error in mapo_forex_perfect: " + err.message);
+  }
+
+  try {
     //await runIndicator();
   } catch (err) {
     //console.error("Error in scanning and logging: ", err);
