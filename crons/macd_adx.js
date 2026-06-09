cc15395cbc03e6b581acfd18fbe5407a11948c50
gaurav <gauravgargtech2@gmail.com>
2026-06-10 00:15:50 +1000

    d

--- crons/macd_adx.js
+++ crons/macd_adx.js
@@ -234,7 +234,7 @@ async function checkMacdAdx() {
       if (isMacdChangeDetected) {
         const latestAdx = last5Adx[last5Adx.length - 1];
         if (
-          latestAdx.adx > 20 &&
+          latestAdx.adx > 15 &&
           (latestAdx.adx > last5Adx[last5Adx.length - 2].adx ||
             latestAdx.adx > last5Adx[last5Adx.length - 3].adx ||
             latestAdx.adx > last5Adx[last5Adx.length - 4].adx)
