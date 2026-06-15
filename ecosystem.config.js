723dad16a14d3db3474e7e75ff100fa0977f8032
gaurav <gauravgargtech2@gmail.com>
2026-06-15 16:03:02 +1000

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,7 @@
 module.exports = {
   apps: [
-    // MAIN API SERVER
     {
-      name: "api",
-      script: "app.js",
-      node_args: "-r newrelic",
-      instances: 1,
-      exec_mode: "fork",
-      env: {
-        NODE_ENV: "prod",
-      },
-      watch: false,
-      max_memory_restart: "300M",
-    },
-    {
-      name: "all_crons",
+      name: "crons",
       script: "./crons/all_crons.js",
       node_args: "-r newrelic",
       instances: 1,
