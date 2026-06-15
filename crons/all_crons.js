79fb5daaf4235941c78eac609e359645e38e4f0b
gaurav <gauravgargtech2@gmail.com>
2026-06-16 02:04:52 +1000

    d

--- crons/all_crons.js
+++ crons/all_crons.js
@@ -1,27 +1,29 @@
 require("../config/config");
 const cron = require("node-cron");
 const populateDataInRedis = require("./populate_data");
+const checkAdxTrend = require("./adx");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc.js");
+const timezone = require("dayjs/plugin/timezone.js");
+const { sendSignalAlert, sendPushNotif } = require("../config/telegram_notify");
+
+/*
+const btcEmaTrending = require("./btc_ema_50_200.js");
 const runIndicator = require("./log_signals_indicator");
 const weekendClose = require("./weekend_close");
 const scanMongoAndFindSignals = require("./place_order");
 const fetchBalance = require("./fetch_balance");
 //require("./place_tp_orders");
 const mapoBtc = require("./mapo_btc");
-const { sendSignalAlert, sendPushNotif } = require("../config/telegram_notify");
 const theForexPerfectMapo = require("./mapo_forex_perfect");
 const closeTo4Hour = require("./close_to_4_hour");
 const runEmaCrossing = require("./ema_crossing");
 const runEmaCrossingSimple = require("./ema_crossing_simple");
 const checkMacdAdx = require("./macd_adx");
-const dayjs = require("dayjs");
-const utc = require("dayjs/plugin/utc.js");
-const timezone = require("dayjs/plugin/timezone.js");
 const checkMacdAdxReversal = require("./macd_adx_reversal");
 const fvgDetector = require("../crons/fvg_detector_forex.js");
-const checkAdxTrend = require("./adx");
-const btcEmaTrending = require("./btc_ema_50_200.js");
-
 const scanAllPairs = require("./fvg_detector_new.js");
+*/
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
