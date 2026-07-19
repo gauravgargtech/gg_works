e5ddca5c643bac60cee1b8ccf9f3c8f262a0765a
gaurav <gauravgargtech2@gmail.com>
2026-07-19 22:53:36 +1000

    changes

--- crons/vortex_forex.js
+++ crons/vortex_forex.js
@@ -3,9 +3,17 @@ const https = require("https");
 
 const { sendPushNotif } = require("../config/telegram_notify");
 
+const { insert } = require("../adapters/mongo");
+
 const { fetchCandles } = require("../exhanges/oanda");
 
 const vortexIndicator = require("../indicators/vortex");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc.js");
+const timezone = require("dayjs/plugin/timezone.js");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -27,16 +35,34 @@ async function checkVortexForex() {
     const currentVortex = vortex[vortex.length - 1];
     const secondLastVortex = vortex[vortex.length - 2];
 
+    const currentTimers = dayjs()
+      .tz("Australia/Brisbane")
+      .format("YYYY-MM-DD HH:mm:ss");
+
     if (
       currentVortex.vip > currentVortex.vim &&
       secondLastVortex.vip < secondLastVortex.vim
     ) {
       await sendPushNotif(`Forex Vortex Crossover 1D : ${symbol} - BULLISH`);
+      await insert("vortex_forex_daily", {
+        symbol,
+        time: currentTimers,
+        timestamp: dayjs().tz("Australia/Brisbane").unix(),
+        vip: currentVortex.vip,
+        vim: currentVortex.vim,
+      });
     } else if (
       currentVortex.vip < currentVortex.vim &&
       secondLastVortex.vip > secondLastVortex.vim
     ) {
       await sendPushNotif(`Forex Vortex Crossover 1D : ${symbol} - BEARISH`);
+      await insert("vortex_forex_daily", {
+        symbol,
+        time: currentTimers,
+        timestamp: dayjs().tz("Australia/Brisbane").unix(),
+        vip: currentVortex.vip,
+        vim: currentVortex.vim,
+      });
     }
   }
   return;
