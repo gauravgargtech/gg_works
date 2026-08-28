5fcec0a4d125a3889940987e51ca8d28db486ad1
gauravgargtech <gauravgargtech2@gmail.com>
2026-08-28 20:33:48 +1000

    subs

--- crons/auto_forex_order.js
+++ crons/auto_forex_order.js
@@ -303,7 +303,7 @@ async function autoForexOrder() {
       let onlyClose = false;
       let placeNew = true;
 
-      if (theCandleSize > 30) {
+      if (theCandleSize > 20) {
         onlyClose = true;
         placeNew = false;
       }
@@ -349,7 +349,7 @@ async function autoForexOrder() {
       let onlyClose = false;
       let placeNew = true;
 
-      if (theCandleSize > 30) {
+      if (theCandleSize > 20) {
         onlyClose = true;
         placeNew = false;
       }
