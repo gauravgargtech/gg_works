e2a937e44f7de45bcc915d0534c454914600518c
gauravgargtech <gauravgargtech2@gmail.com>
2026-09-09 16:21:19 +1000

    bybit sub

--- crons/auto_crypto_order.js
+++ crons/auto_crypto_order.js
@@ -93,7 +93,7 @@ async function autoCryptoOrder() {
     const symbol = pair;
 
     let isSymbolFromPosition = false;
-    if (allPairsFromPosition.includes(symbol)) {
+    if (!fromAPI.includes(symbol)) {
       isSymbolFromPosition = true;
     }
 
