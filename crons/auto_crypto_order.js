14e132610e4ff3904259e72f1ee9b8d0e314220c
gauravgargtech <gauravgargtech2@gmail.com>
2026-09-23 08:25:12 +1000

    15 m

--- crons/auto_crypto_order.js
+++ crons/auto_crypto_order.js
@@ -106,7 +106,7 @@ async function autoCryptoOrder() {
 
     let candles;
     try {
-      candles = await fetchCandles(symbol, 30, 2980);
+      candles = await fetchCandles(symbol, 15, 2980);
     } catch (err) {
       continue;
     }
