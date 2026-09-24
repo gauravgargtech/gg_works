83e1fa5c38df6863671dc99cae1b0cc93ddeabf8
gauravgargtech <gauravgargtech2@gmail.com>
2026-09-25 00:46:54 +1000

    pkama real

--- crons/auto_crypto_order.js
+++ crons/auto_crypto_order.js
@@ -47,7 +47,7 @@ async function autoCryptoOrder() {
     //return;
   }
 
-  const top50Pairs = await getTop100ByVolume(20);
+  const top50Pairs = await getTop100ByVolume(30);
 
   const fromAPI = [];
 
